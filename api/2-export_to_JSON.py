1efa32d54561e8614c0ec4b5786d009ec3aac029
Omar Mohamed <119631276+omarmohamedreda@users.noreply.github.com>
2024-02-29 21:35:02 +0200

    Update 2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,44 +1,56 @@
-"""
-This module contains an api that exports to JSON
-"""
+""" extend Python script to export data in the CSV format.
+
+Requirements:
+
+Records all tasks that are owned by this employee
+Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+File name must be: USER_ID.csv """
+
 
 import json
 import requests
 import sys
 
 
-def get_employee_todo_progress(employee_id):
-    # Fetch employee details
-    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    response = requests.get(employee_url)
-    employee_data = response.json()
-    employee_name = employee_data['username']  # Changed from 'name' to 'username'
+def get_user_data(user_id):
+    url = "https://jsonplaceholder.typicode.com/"
+    user_url = "{}users/{}".format(url, user_id)
+    response = requests.get(user_url)
+    return response.json()
 
-    # Fetch TODO list for the employee
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+
+def get_user_tasks(user_id):
+    url = "https://jsonplaceholder.typicode.com/"
+    todos_url = "{}todos?userId={}".format(url, user_id)
     response = requests.get(todos_url)
-    todos = response.json()
+    return response.json()
 
-    # Create JSON data
-    json_data = {
-        employee_id: [{"task": todo['title'], "completed": todo['completed'], "username": employee_name} for todo in todos]
-    }
 
-    # Create JSON file name
-    json_filename = f"{employee_id}.json"
+def export_to_json(user_id, user_data, tasks):
+    l_task = []
+    for task in tasks:
+        dict_task = {
+            "task": task.get("title"),
+            "completed": task.get("completed"),
+            "username": user_data.get("username"),
+        }
+        l_task.append(dict_task)
 
-    # Write data to JSON file
-    with open(json_filename, 'w') as jsonfile:
-        json.dump(json_data, jsonfile)
+    d_task = {str(user_id): l_task}
+    filename = "{}.json".format(user_id)
 
-   
-    
+    with open(filename, "w") as json_file:
+        json.dump(d_task, json_file, indent=2)
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
         print("Usage: python script.py <employee_id>")
         sys.exit(1)
-    
-    employee_id = int(sys.argv[1])
-    json_filename = get_employee_todo_progress(employee_id)
-    
+
+    user_id = int(sys.argv[1])
+
+    user_data = get_user_data(user_id)
+    user_tasks = get_user_tasks(user_id)
+
+    export_to_json(user_id, user_data, user_tasks)
