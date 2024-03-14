3cc28cfe664bc68041d2a8592550d57302bd273c
Omar Mohamed <119631276+omarmohamedreda@users.noreply.github.com>
2024-03-14 16:28:26 +0200

    Update 2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,29 +1,38 @@
 
-#!/usr/bin/python3
-"""Gather data from an API and export it to a JSON file."""
+"""
+This module makes 2 API requests and 
+uses the information given to write it to a JSON file
+"""
+
 import json
-import requests
+import requests 
 from sys import argv
 
+def get_info(id):
+    url1 = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
+    empurl= f'https://jsonplaceholder.typicode.com/users/{id}'
+    # Get todo tasks
+    res1 = requests.get(url1)
+    data1 = res1.json()
+    # Get employee information
+    res2 = requests.get(empurl)
+    employeedata = res2.json()
+
+    USER_ID = employeedata['id']
+    USERNAME = employeedata['username']
+    TOTAL_NUMBER_OF_TASKS = len(data1)
+    tasks = []
+    for task in data1:
+        tasks.append({"task": task['title'], "completed": task['completed'], "username": USERNAME})
+        
+    dictionary = {USER_ID: tasks}
+    with open(f'{id}.json', 'w') as file:
+        json.dump(dictionary, file, indent=4)
 
 if __name__ == "__main__":
-    if len(argv) != 2 or not argv[1].isdigit():
-        print("Usage: ./2-export_to_JSON.py <USER_ID>")
-        exit()
-
-    user_id = argv[1]
-
-    user_info = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                             .format(user_id)).json()
-    user_name = user_info.get('username')
-
-    todo_list = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                             .format(user_id)).json()
-
-    user_tasks = [{"task": task.get('title'), "completed": task.get('completed'), "username": user_name}
-                  for task in todo_list]
-
-    with open('{}.json'.format(user_id), 'w') as json_file:
-        json.dump({user_id: user_tasks}, json_file)
-
-    print("JSON file generated successfully!")
+    if len(argv) > 1:
+        for arg in argv[1:]:
+            get_info(int(arg))
+    else:
+        for i in range(1, 11):
+            get_info(i)
